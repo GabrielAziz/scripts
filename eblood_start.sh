936122834b11c1c145ccb64841423fcb347f0a42
Gabriel Aziz <gabriel@eblood.com.br>
2022-09-05 08:49:54 -0300

    eblood_start V2 - criaçao das flags -h e -i (help e intertive)

--- eblood_start.sh
+++ eblood_start.sh
@@ -9,20 +9,7 @@ use_red()
   printf "\033[0;31m$*\033[0m";
 }
 
-#### FUNÇOES
-open_tab()
-{
-  gnome-terminal --tab -t "$1" -- bash -c "cd $2; $3; exec zsh";
-}
-
-up_server()
-{
-  for project in ${projects[@]}; do
-    $project;
-  done
-}
-
-#### APLICAÇOES
+#### APLICAÇOES A SEREM SUBIDAS
 bloodbank()
 {
   tabname="BloodBank"
@@ -51,32 +38,91 @@ user()
   open_tab $tabname $path "bundle exec puma";
 }
 
-#### RODANDO
+#### FUNÇOES
+open_tab()
+{
+  gnome-terminal --tab -t "$1" -- bash -c "cd $2; $3; exec zsh";
+}
 
-## QUANDO TEM 0 ARGUMENTOS
-if [ $# -eq 0 ]; then
-  projects=( "bloodbank" "labs" "billing" "user" );
-fi
-
-## QUANDO TEM ENTRE 1 E 4 ARGUMENTOS
-if [ $# -gt 0 ] && [ $# -lt 5 ]; then
-  for i in $*; do
-    if [ $i == "bloodbank" ] || [ $i == "labs" ] || [ $i == "billing" ] || [ $i == "user" ]; then
-      projects+=( $i );
-    else
-      echo "Erro ao conectar com o servidor $(use_red $i), apenas as opçoes $(use_red [bloodbank, labs, billing, user])";
-      exit 1;
-    fi
+up_server()
+{
+  projects+=( "user" );
+  for project in ${projects[@]}; do
+    $project;
   done
-fi
+  kill -9 $PPID;
+}
 
-## QUANDO TEM MAIS DE 4 ARGUMENTOS
-if [ $# -gt 4 ]; then
-  echo "Argumentos demais, $(use_red maximo 4 argumentos)";
-  exit 1;
-fi
 
-## SUBINDO APLICAÇOES
+capture_args()
+{
+  ## QUANDO TEM 0 ARGUMENTOS
+  if [ $# -eq 0 ]; then
+    projects=( "bloodbank" "labs" "billing" );
+  fi
+
+  ## QUANDO TEM ENTRE 1 E 3 ARGUMENTOS
+  if [ $# -gt 0 ] && [ $# -lt 4 ]; then
+    for i in $*; do
+      if [ $i == "bloodbank" ] || [ $i == "labs" ] || [ $i == "billing" ] ; then
+        projects+=( $i );
+      else
+        echo "Erro ao conectar com o servidor $(use_red $i), apenas as opçoes $(use_red [bloodbank, labs, billing])";
+        exit 1;
+      fi
+    done
+  fi
+
+  ## QUANDO TEM MAIS DE 4 ARGUMENTOS
+  if [ $# -gt 4 ]; then
+    echo "Argumentos demais, $(use_red maximo 4 argumentos)";
+    exit 1;
+  fi
+
 up_server;
-kill -9 $PPID;
+}
+
+#### CAPTURANDO ARGUMENTOS
+while getopts 'ih' OPTION; do
+  case "$OPTION" in
+    i)
+      ##BloodBank
+      echo "deseja subir $(use_red bloodbank)?(y/n)"
+      read -r bloodbank_up
+      if [ $bloodbank_up != "y" ] && [ $bloodbank_up != "n" ]; then echo "resposta invalida, apenas s/n"; exit 1; fi
+
+      ##Labs
+      echo "deseja subir $(use_red labs)?(y/n)"
+      read -r labs_up
+      if [ $labs_up != "y" ] && [ $labs_up != "n" ]; then echo "resposta invalida, apenas s/n"; exit 1; fi
+
+      ##Billing
+      echo "deseja subir $(use_red billing)?(y/n)"
+      read -r billing_up
+      if [ $billing_up != "y" ] && [ $billing_up != "n" ]; then echo "resposta invalida, apenas s/n"; exit 1; fi
+
+
+      if [ $bloodbank_up = "y" ]; then projects+=( "bloodbank" ); fi
+      if [ $labs_up = "y" ]; then projects+=( "labs" ); fi
+      if [ $billing_up = "y" ]; then projects+=( "billing" ); fi
+      up_server;
+
+      exit 1;
+      ;;
+    h)
+      echo "Voce pode apenas dar eblood_start e subir $(use_red [bloodbank, labs, billing e user])"
+      echo "Mas se voce prefere argumentos pode passa-los, sera aceito $(use_red [bloodbank, labs, billing])"
+      echo "Ou passar a flag $(use_red -i) para escolher interativamente quais subir"
+      exit 1;
+      ;;
+    ?)
+      echo "$(use_red Flag desconhecida:) use $(use_red [-h]) para acessar o tutorial "
+      exit 1;
+      ;;
+  esac
+done
+
+
+#### RODANDO
+capture_args $*;
 
